6c264c7d23cf226a0f780c679b389441de794d5b
Zhasulan <110722383+pecesool@users.noreply.github.com>
2022-08-06 23:38:35 +0600

    delete trash

--- addclass.cs
+++ addclass.cs
@@ -2,12 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
-using System.Data.OleDb;
+
 using MySql.Data.MySqlClient;
 
 namespace DPI
